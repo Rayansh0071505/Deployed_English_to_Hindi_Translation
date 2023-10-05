ec87572caff12087d8e03e223e364183969037da
Rayansh <rayanshsrivastava1@gmail.com>
2023-10-05 10:20:03 +0530

    push

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from transformers import pipeline
 import streamlit as st
 
-# Load the translation pipeline
-pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-hi")
+# @st.cache_data(allow_output_mutation=True) 
+@st.cache_data()
+def load_translation_pipeline():
+    return pipeline("translation", model="Helsinki-NLP/opus-mt-en-hi")
 
 def main():
     st.title('Translation English-Hindi')
     english_sentence = st.text_area("English Sentence", height=100)
+
+    pipe = load_translation_pipeline()
+
     if st.button('Translate'):
         translated_text = pipe(english_sentence, max_length=50)[0]['translation_text']
 
